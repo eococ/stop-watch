8dae3202b801761873f5377cf54b134a076361fe
eococ <kllh@hanmail.net>
2022-04-08 01:07:14 +0900

    Update stopWatch.js

--- stop-watch/WebContent/stopWatch.js
+++ stop-watch/WebContent/stopWatch.js
@@ -27,11 +27,15 @@
 		requestId = requestAnimationFrame(start);
 	};
 	
+	
+	
 	//스톱워치 일시정지
 	function pause(){
 		cancelAnimationFrame(requestId);
 	};
 
+	
+	
 	//스톱워치 종료
 	function stop(id){
 		cancelAnimationFrame(start);
@@ -40,12 +44,16 @@
 		$("#" + id).html(time);
 	}
 	
+	
+	
 	//리셋
 	function reset(){
 		ms = 0;
 		seconds = 0;
 		minutes = 0;
 	}
+	
+	
 	
 	//현재 시간 가져오기
 	function getTime(){
@@ -59,6 +67,53 @@
 		
 		return nowTime;
 	}
+	
+	
+	
+	var callbacks = {
+			update : []
+		  , alarm : []
+	}
+	
+	//이벤트 등록
+	function on(eventName, callback){
+		
+		if(this.callbacks[eventName] === undefined){
+			return false;
+		}
+		
+		this.callbacks[eventName] = callback;
+		
+		return true;
+	}
+	
+	
+	
+	//이벤트 해제
+	function off(eventName){
+		
+		this.callbacks[eventName] = [];
+		
+		return true;
+	}
+	
+
+
+	//알람 이벤트
+	function alarm(setAlarm){
+		
+		//현재시간
+		var now = getTime();
+
+		if(now === setAlarm){
+			//알람 울림...... 어떻게 해야할지... 모르곘어요...
+			//..........ㅠ
+		}
+		
+	}
+	
+	
+	
 	
 	
 	
